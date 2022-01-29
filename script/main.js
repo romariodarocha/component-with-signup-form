7f8c8c6b6e6416416b6e49cadf575be2dfd9f26a
Da Rocha <93994617+romariodarocha@users.noreply.github.com>
2022-01-29 08:37:38 -0300

    final project [4]

--- script/main.js
+++ script/main.js
@@ -1,35 +1,40 @@
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const email = document.getElementById('email');
-const submitButton = document.querySelector('button[type="submit"]');
-let emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const inputs = document.getElementsByClassName('input-text');
+const icons = document.getElementsByClassName('error-icon');
+const labels = document.getElementsByTagName('label');
 
-submitButton.addEventListener('click', function(e) {
-    e.preventDefault();
+// Patern to validate e-mail
+const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (firstName.value === '') {
-        firstName.value = '';
-        showErrorMessage(firstName, "First Name cannot be empty!")
+//function to validate email
+function emailIsValid(email) {
+    if (email.checkValidity() && pattern.test(email.value.toLowerCase())) {
+      return true;
+    } else {
+        return false;
     }
-
-    if (lastName.value === '') {
-        showErrorMessage(lastName, "Last Name cannot be empty!")
-    }
-
-    if (email.value === '') {
-        showErrorMessage(email, "Looks like this is not an email!")
-    } else if (!emailRegExp.test(email.value)) {
-        showErrorMessage(email, "Looks like this is not an email!");
-    }
-
-    if (password.value === '') {
-        showErrorMessage(password, "Password cannot be empty");
-    }
-});
-
-function showErrorMessage(inputValue, message) {
-    const inputControl = inputValue.parentElement;
-    inputControl.classList.add('error');
-    const errorMessage = inputControl.querySelector('.error-text');
-    errorMessage.innerHTML = message;
 }
+
+//remove the error message when the users changes anything in the input field
+//Use the input event because focus conflicts to browser's error message itself
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', () => {
+      inputs[i].classList.remove('input-error');
+      icons[i].classList.remove('error-icon-error');
+      labels[i].classList.remove('label-error');
+    });
+}
+
+//show error message if any of input fields are empty or if the email is invalid
+document.getElementById('submit-button').addEventListener('click', () => {
+    for (let i in inputs) {
+      let isValid = true;
+      if (inputs[i].id == 'email') {
+        isValid = emailIsValid(inputs[i]);
+      }
+      if (inputs[i].value == '' || !isValid) {
+        inputs[i].classList.add('input-error');
+        icons[i].classList.add('error-icon-error');
+        labels[i].classList.add('label-error');
+      }
+    }
+  });
